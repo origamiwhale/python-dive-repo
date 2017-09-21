de8a7cbdf4596202b0da4cb7bcd457f2e916e03c
origamiwhale <mokosa.j@gmail.com>
2017-09-21 14:54:54 +0200

    Removed whitespaces and renamed module

--- first_program_01.py
+++ first_program_01.py
@@ -20,7 +20,9 @@ def approximate_size(size, a_kilobate_is_1024_bytes=True):
 
     if size < 0:
         raise ValueError('size must be non-negative')
+
     multiple = 1024 if a_kilobate_is_1024_bytes else 1000
+
     for suffix in SUFFIXES[multiple]:
         size /= multiple
         if size < multiple:
